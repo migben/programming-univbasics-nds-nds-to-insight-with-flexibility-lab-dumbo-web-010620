41c552e623415d2f815b4e8d2ac942f121a22446
Miguel Ben <itsmigben@gmail.com>
2019-12-12 13:27:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -112,7 +112,7 @@ def movies_with_directors_set(src)
   
   while idx < src.size
     
-    dir_movies.push()
+    dir_movies.push(movies_with_director_key(src[idx][:name]))
     
     idx += 1
   end
