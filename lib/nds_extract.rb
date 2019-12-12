ef78a442a981d1c808cc115ce30b776e66712c7f
Miguel Ben <itsmigben@gmail.com>
2019-12-12 13:26:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -107,7 +107,7 @@ def movies_with_directors_set(src)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
-  dir_movies = []
+  dir_movies = [ ]
   
 end
 
